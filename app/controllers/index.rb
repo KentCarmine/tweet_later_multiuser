7747ba6183668cfd47e3e51487f316c9a9502990
Apprentice <apprentice@dbc-chicago031.local>
2013-11-13 08:16:59 -0600

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,44 @@
+get '/' do
+  erb :index
+end
+
+get '/sign_in' do
+  # the `request_token` method is defined in `app/helpers/oauth.rb`
+  redirect request_token.authorize_url
+end
+
+get '/sign_out' do
+  session.clear
+  redirect '/'
+end
+
+get '/auth' do
+  # the `request_token` method is defined in `app/helpers/oauth.rb`
+  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  # our request token is only valid until we use it to get an access token, so let's delete it from our session
+  session.delete(:request_token)
+
+  # at this point in the code is where you'll need to create your user account and store the access token
+  current_user = User.find_or_create_by_username(
+    :username => @access_token.params[:screen_name],
+    :oauth_token => @access_token.token,
+    :oauth_secret => @access_token.secret
+    )
+
+  session[:current_user] = current_user
+
+  erb :index
+end
+
+post '/' do
+  @twitter_username = session[:current_user].username
+
+  twitter_user = Twitter::Client.new(
+    :oauth_token => session[:current_user].oauth_token,
+    :oauth_token_secret => session[:current_user].oauth_secret
+  )
+
+  twitter_user.update(params[:tweet_text])
+
+  redirect to '/'
+end
---
+++ public/js/application.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#tweet_form").on("submit", function(event){
+    event.preventDefault();
+
+    $(".success-msg").remove();
+
+    $(".tweet_form_elements").prop("disabled", true);
+    $(".container").append("<h4 class='wait-msg'>Tweeting, please wait...</h4>");
+
+    var data = { "tweet_text": $("#tweet-box").val() }
+
+    $.post("/", data, function(response) {
+      $(".tweet_form_elements").prop("disabled", false);
+      $(".wait-msg").remove();
+      $(".container").append("<h4 class='success-msg'>Tweet sent successfully!</h4>");
+    });
+  });
+});
